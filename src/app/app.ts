6dce7a0f2bccfcb6bcad5822b8887d913976945e
Ajmal O M <amohamed@suyati.com>
2025-09-22 17:35:18 +0530

    feat: add features and testimonials components with styles and templates

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './components/header/header.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { FeaturesComponent } from './components/features/features.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgbModule, HeaderComponent, HeroComponent],
+  imports: [RouterOutlet, NgbModule, HeaderComponent, HeroComponent, FeaturesComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

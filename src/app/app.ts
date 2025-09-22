12b264fca41bdcc5a8afdfd2628e65d71b6f060a
Ajmal O M <amohamed@suyati.com>
2025-09-22 17:42:45 +0530

    feat: add testimonials component with styles and integrate into app

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,11 +4,12 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './components/header/header.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { FeaturesComponent } from './components/features/features.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgbModule, HeaderComponent, HeroComponent, FeaturesComponent],
+  imports: [RouterOutlet, NgbModule, HeaderComponent, HeroComponent, FeaturesComponent, TestimonialsComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
